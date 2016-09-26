14ff98a463025b2dee115f3332118ba0a4b1c755
Weierstrass <andreas@bulling.se>
2016-09-26 21:29:52 +0200

    Remove accidentally added files

--- CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/andreasbulling/prog/cpp_template_metaprogramming_exercises
-# Build directory: /Users/andreasbulling/prog/cpp_template_metaprogramming_exercises
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(template_test "/Users/andreasbulling/prog/cpp_template_metaprogramming_exercises/test/template_exercises_test")
-subdirs(src)
-subdirs(test)
